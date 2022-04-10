75c50bb3d3598a2df34cf60283bfb97d3b28c5c2
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-04-10 13:37:41 +0900

    Update file_picker_service.dart

--- lib/screens/learning/file_picker_service.dart
+++ lib/screens/learning/file_picker_service.dart
@@ -6,7 +6,7 @@ import 'package:file_picker/file_picker.dart';
 class FilePickerService {
   Future<Uint8List> imageFilePickAsBytes() async {
     FilePickerResult result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
+      type: FileType.custom, withData: true,
       allowedExtensions: ['jpg', 'png']
     );
 
