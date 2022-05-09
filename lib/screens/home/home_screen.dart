b18b52a75252bafd9ad3605fb7ca6508f93c8a98
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-05-09 18:07:52 +0900

    Update home_screen.dart

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -26,7 +26,7 @@ class HomeScreen extends StatelessWidget {
         icon: SvgPicture.asset("assets/icons/back.svg"),
         onPressed: () {},
       )
-      /*
+
       actions: <Widget>[
         IconButton(
           icon: SvgPicture.asset(
@@ -49,7 +49,7 @@ class HomeScreen extends StatelessWidget {
         SizedBox(width: kDefaultPaddin / 2)
       ],
 
-       */
+
     );
   }
 }
