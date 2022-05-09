7839a27e227fae1e5b69c695eab6781099d0615e
Chanhee Oh <77625823+kozeldark@users.noreply.github.com>
2022-05-09 19:00:18 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:re_mind/constants.dart';
 import 'package:re_mind/screens/home/home_screen.dart';
+import 'package:re_mind/screens/login/signup.dart';
+import 'package:re_mind/screens/login/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:re_mind/screens/login/splash.dart';
+import 'package:re_mind/screens/login/auth.dart';
+import 'package:re_mind/screens/home/home_screen.dart';
+import 'package:re_mind/screens/login/profile.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  Firebase.initializeApp(
+    options: FirebaseOptions(
+      apiKey: "XXX", // Your apiKey
+      appId: "XXX", // Your appId
+      messagingSenderId: "XXX", // Your messagingSenderId
+      projectId: "XXX", // Your projectId
+    ),
+  );
+    runApp(MyApp());
+  }
+
+  class MyApp extends StatelessWidget {
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: HomeScreen(),
-    );
+  return MaterialApp(
+  debugShowCheckedModeBanner: false,
+  title: 'Flutter & Firebase',
+  theme: ThemeData(
+  textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+  visualDensity: VisualDensity.adaptivePlatformDensity,
+  primarySwatch: Colors.indigo,
+  ),
+  initialRoute: SplashPage.routeName,
+  routes: {
+  AuthPage.routeName: (context) => AuthPage(),
+  SignInPage.routeName: (context) => SignInPage(),
+  SignUpPage.routeName: (context) => SignUpPage(),
+  },
+  onGenerateRoute: (settings) {
+  switch (settings.name) {
+  case HomeScreen.routeName: {
+  return MaterialPageRoute(
+  builder: (context) => HomeScreen()
+  );
+  } break;
+
+  case ProfilePage.routeName: {
+  return MaterialPageRoute(
+  builder: (context) => ProfilePage(user: settings.arguments)
+  );
+  } break;
+
+  default: {
+  return MaterialPageRoute(
+  builder: (context) => SplashPage()
+  );
+  } break;
   }
-}
+  },
+  );
+  }
+  }
